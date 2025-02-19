86b45faf0ffee595350fcbea664740f4f88eb8aa
Kelven Bragoa <kelvenbragoa@hotmail.com>
2025-02-19 10:35:05 +0200

    first: ocmmit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,17 +21,17 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         // $this->call(ProductTableSeeder::class);
-        // $this->call(DepartmentTableSeeder::class);
-        // $this->call(CategoriesTableSeeder::class);
-        // $this->call(UserTableSeeder::class);
-        // $this->call(OrderStatusTableSeeder::class);
-        // $this->call(PaymentMethodTableSeeder::class);
-        // $this->call(ReservationStatusTableSeeder::class);
-        // $this->call(RoleTableSeeder::class);
-        // $this->call(TablesTableSeeder::class);
-        // $this->call(TableStatusTableSeeder::class);
-        // $this->call(OrderItemStatusTableSeeder::class);
-        // $this->call(StatusTableSeeder::class);
+        $this->call(DepartmentTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(OrderStatusTableSeeder::class);
+        $this->call(PaymentMethodTableSeeder::class);
+        $this->call(ReservationStatusTableSeeder::class);
+        $this->call(RoleTableSeeder::class);
+        $this->call(TablesTableSeeder::class);
+        $this->call(TableStatusTableSeeder::class);
+        $this->call(OrderItemStatusTableSeeder::class);
+        $this->call(StatusTableSeeder::class);
         $this->call(CashRegisterTableSeeder::class);
 
 
